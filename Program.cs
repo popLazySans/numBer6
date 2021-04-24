6f96195fc5c94d4ab6d90f06e0de693e76871d4c
IamPimdedd <hasamu.44@gmail.com>
2021-04-24 14:50:19 +0700

    no message

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prefab = Console.ReadLine();
+            int Lenge = int.Parse(Console.ReadLine());
+            int high = int.Parse(Console.ReadLine());
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i =1;i <= num; i++)
+            {
+                for(int j = 1; j <= high; j++)
+                {
+                    for (int k = 1; k<=Lenge*i; k++)
+                    {
+                        Console.Write(prefab);
+                    }
+                    Console.WriteLine("");
+                }
+                
+            }
+        }
+    }
+}
